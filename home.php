2f7d60118dc775d5f46c296ce4712cf97b7fc1ce
Yuuki175 <realouise175@gmail.com>
2024-08-27 16:04:55 +0800

    draft

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php get_header()?>
+
+    <section class="blogBanner bg--primary">
+      <div class="container">
+        <div class="blogBanner__wrapper">
+
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+          <div class="blogBanner__content">
+            <?php if(has_post_thumbnail( )) {
+                the_post_thumbnail();
+            }?>
+            <h3><?php the_title()?></h3>
+            <p>
+             <?php echo wp_trim_words(get_the_excerpt(), 10)?>
+            </p>
+            <a href="<?php the_permalink()?>" class="btn bg--secondary">Read More</a>
+          </div>
+
+        <?php endwhile;
+        else : 
+            echo "No More blog";
+        endif;
+        ?>
+
+        </div>
+      </div>
+    </section>
+<?php get_footer()?>
---
+++ page-rice.php
@@ -0,0 +1,30 @@
+<?php get_header() ?>
+
+<section class="food bg--dark">
+    <div class="container">
+        <h1>Rice Meals</h1>
+        <div class="food_wrapper">
+            <?php $rice = new WP_Query(array(
+                'post_type' => 'rice',
+                'posts_per_page' => -1
+            ))?>
+
+        <?php if($rice->have_posts()) : while($rice->have_posts()) : $rice->the_post(); ?>
+            <div class="food_item">
+                <?php if(has_post_thumbnail()) {
+                    the_post_thumbnail();
+                }?>
+                <h3><?php the_title() ?></h3>
+                <p><?php echo get_post_meta(get_the_ID(), 'Price', true)?></p>
+                <a href="#" class="btn bg--secondary">Add to cart</a>
+            </div>
+        <?php endwhile;
+            else:
+                echo "no rice";
+            endif; 
+        ?>
+
+        </div>
+    </div>
+</section>
+<?php get_footer() ?>
